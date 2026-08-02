08467310660c0273c2d6228f3c7a352fb1446c4f
jerome-byte <akotayaojerome@gmail.com>
2026-08-02 12:49:01 +0000

    correction du port d'ecoute render

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'https://back-office-blush-mu.vercel.app/',
+      'https://back-office-blush-mu.vercel.app',
       'http://localhost:5173',
       'http://localhost:3000',
     ],
@@ -14,7 +14,8 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000);
-  console.log('🚀 Serveur démarré sur le port 3000');
+  const port = process.env.PORT || 3000;
+await app.listen(port);
+console.log(`🚀 Serveur démarré sur le port ${port}`);
 }
 bootstrap();
